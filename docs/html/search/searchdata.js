19375058251d4eb2b0fdc18210d547d0fbb6985c
Juan C Martinez <jmartinez29@luc.edu>
2024-12-02 10:38:21 -0600

    Documentation will be generated locally; amended documentation error.

--- docs/html/search/searchdata.js
+++
@@ -1,6 +0,0 @@
-var indexSectionsWithContent =
-    {0: 'bcfglmprstu', 1: 'fmpr', 2: 'cglmst', 3: 'bflmpu'};
-
-var indexSectionNames = {0: 'all', 1: 'files', 2: 'functions', 3: 'defines'};
-
-var indexSectionLabels = {0: 'All', 1: 'Files', 2: 'Functions', 3: 'Macros'};
